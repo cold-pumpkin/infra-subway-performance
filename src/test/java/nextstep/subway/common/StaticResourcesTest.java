9649b246f708f93809e68958092ad4090e2a2fbd
Philip <pch4623@gmail.com>
2022-07-09 15:14:19 +0900

    refactor: 테스트 파일 수정

--- src/test/java/nextstep/subway/common/StaticResourcesTest.java
+++ src/test/java/nextstep/subway/common/StaticResourcesTest.java
@@ -10,6 +10,8 @@ import org.springframework.http.CacheControl;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.concurrent.TimeUnit;
+
 import static nextstep.subway.common.WebMvcConfig.PREFIX_STATIC_RESOURCES;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,13 +21,11 @@ public class StaticResourcesTest {
     @Autowired
     private WebTestClient client;
 
-    @DisplayName("정적 리소스 캐싱 테스트")
     @Test
-    void get_static_resources() {
+    void get_static_js_resources() {
         String uri = PREFIX_STATIC_RESOURCES + "/static/js/main.js";
         EntityExchangeResult<String> response = client
-                .get()
-                .uri(uri)
+                .get().uri(uri)
                 .exchange()
                 .expectStatus()
                 .isOk()
@@ -43,5 +43,26 @@ public class StaticResourcesTest {
                 .exchange()
                 .expectStatus()
                 .isNotModified();
+    }
+
+    @Test
+    void get_static_css_resources() {
+        String uri = PREFIX_STATIC_RESOURCES + "/static/css/main.css";
+        EntityExchangeResult<String> response = client
+                .get().uri(uri)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader()
+                .cacheControl(CacheControl.maxAge(60 * 60 * 24 * 365, TimeUnit.SECONDS))
+                .expectBody(String.class)
+                .returnResult();
+
+        String etag = response.getResponseHeaders()
+                .getETag();
+
+        client.get().uri(uri)
+                .header("If-None-Match", etag)
+                .exchange()
+                .expectStatus().isNotModified();
     }
 }
